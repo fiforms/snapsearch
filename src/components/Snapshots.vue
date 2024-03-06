d1309c16d906c32ad493037b7753da8d8311625a
Daniel McFeeters <fiforms@gmail.com>
2024-03-06 14:28:33 -0500

    Got the search box working and a good start on styling

--- src/components/Snapshots.vue
+++ src/components/Snapshots.vue
@@ -1,29 +1,44 @@
 <template>
   <div class="container">
-	  <div class="parametercontainer"> Snapshot Search: <input v-model="searchterm" name="search" /> Sort: 
+	  <div class="parametercontainer">
+	    <div class="homelink">
+
+	      <a href="https://snapshots.vrbm.org">
+	      <img src="/arrow.png" />
+	      Virtual Bible <br />Snapshot Project
+	      </a>
+	    </div>
+	    <div class="searchbox">
+	    <input v-model="searchterm" id="searchinput" /><br />
+	    
 	    <label>
-	      <input name="sortradio" type="radio" v-model="sort" value="path" @change="reSort">
-	      Sort By File Path
+	      <input name="sortradio" type="radio" v-model="sort" value="path">
+	      Folder Older
 	    </label>
 
 	    <label>
-	      <input name="sortradio" type="radio" v-model="sort" value="date" @change="reSort">
-	      Sort By Date
+	      <input name="sortradio" type="radio" v-model="sort" value="date">
+	      Newest First
 	    </label>
 
 	    <label>
-	      <input name="sortradio" type="radio" v-model="sort" value="random" @change="reSort">
-	      Random Sort
+	      <input name="sortradio" type="radio" v-model="sort" value="random" @click="reSort">
+	      Random
 	    </label>
+	    </div>
+	    <a href="https://snapshots.vrbm.org/"><img src="/searchicon.png" class="logo" /></a>
 	  </div>
 	  <div class="searchcontainer">
-		  <div class="searchtile" v-for="row in searchresults">
+		  <div class="searchtile" v-for="row in searchresults" @click="setHit(row)">
 			  <img class="searchthumb" v-bind:src="'thumbs/' + row.letter + '/' + row.md5 + '.webp'" @click="setHit(row)" />
 		  </div>
 	  </div>
           <div class="overlay" v-if="hit" @click="clearHit()">&nbsp;</div>
 	  <div class="hitdetails" v-if="hit">
+	      <div class="heading">Details
 	      <span class="close" @click="clearHit()">X</span>
+	      </div>
+	      <div class="innerbody">
 	                    <img v-bind:src="'thumbs/' + hit.letter + '/' + hit.md5 + '.768.webp'" /><br />
 			    {{hit.filename}}<br />
 			    {{hit.desc}}<br />
@@ -33,6 +48,7 @@
 			    <a v-if="hit.sourceurl" :href="hit.sourceurl" target="_blank">Image Source</a><br />
 			    <a :href="hit.meddirlink" target="_blank">Browse Folder (Medium Resolution)</a><br />
 			    <a :href="hit.bigdirlink" target="_blank">Browse Folder (High Resolution)</a><br />
+	      </div>
 	  </div>
 		  
 
@@ -50,13 +66,18 @@
 	searchterm: "",
 	searchresults: null,
 	hit: null,
-	sort: "",
+	sort: "random",
 	randomness: [1,2,3,4],
       };
     },
     watch: {
-	    searchterm(searchterm) {
+	    searchterm(newSearchterm) {
 		    this.updateSearch();
+		    this.updateUrl();
+	    },
+	    sort(newSort) {
+	            this.updateUrl();
+	            this.reSort();
 	    }
     },
     methods: {
@@ -75,19 +96,21 @@
         updateSearch (event) {
 	    var res = new Array();
 	    var count = 0
-	    for(let i = 0; i < this.snapshots.length; i++) {
-		    let terms = this.searchterm.toLowerCase().split(" ")
-		    if(this.match(terms,i)) {
-			    let row=this.snapshots[i];
-			    row.letter = row.md5.substring(0,1);
-			    res.push(row);
-			    count++
+	    if(this.snapshots) {
+		    for(let i = 0; i < this.snapshots.length; i++) {
+			    let terms = this.searchterm.toLowerCase().split(" ")
+			    if(this.match(terms,i)) {
+				    let row=this.snapshots[i];
+				    row.letter = row.md5.substring(0,1);
+				    res.push(row);
+				    count++
+			    }
+			    if(count >= 100) {
+				break;
+			    }
 		    }
-		    if(count >= 100) {
-			break;
-		    }
+		    this.searchresults = res;
 	    }
-	    this.searchresults = res;
 	},
 	setHit(row) {
 	    this.hit = row;
@@ -128,73 +151,148 @@
 	        return num
 	    }
 	},
+        updateUrl() {
+          // Update the URL with the current search parameter
+          const baseUrl = window.location.href.split('#')[0];
+          window.location.href = `${baseUrl}#q=${this.searchterm}&sort=${this.sort}`;
+        },
     },
     created: function() {
-      axios
-        .get('/thumbs/snapshots.json')
-        .then(res => {
-          this.snapshots = res.data;
-        })
-    }
+        // Initialize search based on URL parameter
+        const urlParams = new URLSearchParams(window.location.hash.slice(1));
+        const searchFromUrl = urlParams.get('q');
+        if (searchFromUrl) {
+          this.searchterm = searchFromUrl;
+        }
+        axios
+          .get('/thumbs/snapshots.json')
+          .then(res => {
+            this.snapshots = res.data;
+            this.reSort();
+          })
+    },
   }
 </script>
 
 <style>
-  .parametercontainer {
-    margin-bottom: 5%;
-  }
-  .searchcontainer
-  {
-      background-color: #ddddff;
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-between; /* Adjust as needed */
-      align-content: space-between; /* Adjust as needed */
-      width: 900px;
-  }
-  .searchtile
-  {
-      width: 276px;
-      height: 276px;
-      margin: 5px; /* Adjust as needed */
-      box-sizing: border-box;
-      cursor: pointer;
-      background-color: #999999;
-  }
-  .searechtile:hover
-  {
-      background-color: #666666;
-  }
-  
-    .hitdetails {
-      position: fixed;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      padding: 20px;
-      background-color: #fff;
-      border: 1px solid #ccc;
-      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-      z-index: 1000;
-      max-height: 95vh; /* Set the maximum height as a percentage of the viewport height */
-      overflow-y: auto; /* Enable vertical scrollbar when content exceeds the max height */
+	.parametercontainer {
+		display: block;
+		background-color: #222255;
+		width: 100%;
+		min-height: 4em;
+		color: #cccccc;
+		padding: 0px;
+	}
+	.searchbox {
+	        padding: 1em;
+	        padding-right: 5em;
+	}
+	.searchcontainer
+	{
+		background-color: #ddddff;
+		display: flex;
+		flex-wrap: wrap;
+		width: 100%;
+		padding: 2em;
+	}
+	#searchinput {
+	        width: 50vw;
+	}
+	.logo
+	{
+	       position: absolute;
+	       top: 2px;
+	       right: 2px;
+	       width: 3.5em;
+	}
+	.searchtile
+	{
 
-    }
-    /* Add a close button if needed */
-    .close {
-      position: absolute;
-      top: 10px;
-      right: 10px;
-      cursor: pointer;
-    }
-.overlay {
-  display: block;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.5); /* Semi-transparent grey background */
-  z-index: 999; /* Z-index lower than modal to ensure it's behind */
-}
+		width: 276px;
+		height: 276px;
+		margin: 5px; /* Adjust as needed */
+		box-sizing: border-box;
+		cursor: pointer;
+		background-color: #333333;
+	}
+	.searchtile:hover
+	{
+		background-color: #666666;
+	}
+	.searchtile img
+	{
+		position: relative;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	.hitdetails {
+		position: fixed;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		padding: 0px;
+		background-color: #fff;
+		border: 1px solid #ccc;
+		box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+		z-index: 1000;
+		max-width: 90vw;
+	}
+	.hitdetails .heading {
+		width: 100%;
+		background-color: #000000;
+		height: 2em;
+		color: #ffffff;
+		font-weight: bold;
+		padding: 0.5em;
+	}
+	.hitdetails .innerbody {
+		max-height: 90vh; /* Set the maximum height as a percentage of the viewport height */
+		overflow-y: auto; /* Enable vertical scrollbar when content exceeds the max height */
+		overflow-x: clip;
+		max-width: 60vh;
+
+	}
+	.hitdetails .innerbody img {
+	        width: 100%;
+		max-width: 100%;
+	}
+	
+	/* Add a close button */
+	.close {
+		position: absolute;
+		top: 5px;
+		right: 5px;
+		cursor: pointer;
+		font-weight: bold;
+		color: #999999;
+	}
+	
+	.overlay {
+		display: block;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: rgba(0, 0, 0, 0.5); /* Semi-transparent grey background */
+		z-index: 999; /* Z-index lower than modal to ensure it's behind */
+	}
+	.homelink {
+	    float: left;
+	    width: 15em;
+	    padding-right: 15px;
+	    padding: 6px;
+	    font-size: 9.5pt;
+	}
+	.homelink a {
+	    color: #cccccc;
+	    text-decoration: none;
+	}
+	.homelink img {
+	    width: 2em;
+	    float: left;
+	    margin-right: 1em;
+	}
 </style>
